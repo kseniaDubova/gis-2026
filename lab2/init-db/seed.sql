59f48571113dbf66b800ded7bfc660433f3cf521
kseniaDubova <ksushadubova@icloud.com>
2026-03-19 15:59:02 +0400

    fix

--- lab2/init-db/seed.sql
+++ lab2/init-db/seed.sql
@@ -9,6 +9,7 @@ SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
+CREATE EXTENSION IF NOT EXISTS postgis;
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
